efd99b595b195795b3d68fec6c790cd4e33f3c03
Vohidjon <vohidjonboyqoziyev@yahoo.com>
2024-07-09 16:25:53 +0500

    all done

---
+++ customer/views/customers.py
@@ -0,0 +1,73 @@
+import csv
+import json
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.db.models.functions import Cast
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.db.models import Q, TextField
+from customer.forms import CustomerModelForm
+from customer.models import Customer
+
+
+# Create your views here.
+
+
+def customers_page(request):
+    customers = Customer.objects.all()
+    paginator = Paginator(customers, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    search_query = request.GET.get('search')
+    if search_query:
+        customers = Customer.objects.filter(
+            Q(full_name__icontains=search_query) | Q(address__icontains=search_query))
+    context = {
+        'customers': customers,
+        'page_obj': page_obj,
+        'search_query': search_query
+    }
+    return render(request, 'customer/customer-list.html', context)
+
+
+def add_customer(request):
+    form = CustomerModelForm()
+    if request.method == 'POST':
+        form = CustomerModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('customers')
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'customer/add-customer.html', context)
+
+
+def delete_customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    if customer:
+        customer.delete()
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            'Customer successfully deleted'
+        )
+        return redirect('customers')
+
+
+def edit_customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    form = CustomerModelForm(instance=customer)
+    if request.method == 'POST':
+        form = CustomerModelForm(instance=customer, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+
+            return redirect('customers')
+    context = {
+        'form': form,
+    }
+    return render(request, 'customer/update-customer.html', context)
